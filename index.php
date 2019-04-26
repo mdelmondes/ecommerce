3f0c8b95609e50f802e35f7949ed062a229eac47
Matheus Delmondes <mdelmondes@icloud.com>
2019-04-26 00:09:44 -0300

    inclusão de autoload e classes

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
+<?php 
 
-	echo "Site ecommerce";
+require_once("vendor/autoload.php");
 
-?>
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Hcode\DB\Sql();
+
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
+
+});
+
+$app->run();
+
+ ?>
